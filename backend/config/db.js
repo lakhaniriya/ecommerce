cc29168f5976a48acaa5567af656d78bcc25c96c
riya lakhani <riyalakhani694@gmail.com>
2026-06-30 09:42:48 +0530

    -fix bugs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
-import * as dotenv from 'dotenv';
-dotenv.config()
-export const  connnetDb = async() =>{
-try{
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const connnetDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-}catch(err){
-
-}
-}
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+    process.exit(1);
+  }
+};
