b82b5e388107fa66951853b5854e4af424d32e2b
riya lakhani <riyalakhani694@gmail.com>
2026-06-30 09:27:45 +0530

    -fix bug

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const server = http.createServer(app);
 
 connnetDb();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 1000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
